0214fce86b86628dd39f0c79ddf07b820f276604
Miguel26112001 <u202220294@upc.edu.pe>
2025-10-12 20:17:08 -0500

    chore: added author commands

---
+++ src/main/java/pe/edu/upc/center/autores_platform/authoring/domain/model/commands/DeleteAuthorCommand.java
@@ -0,0 +1,4 @@
+package pe.edu.upc.center.autores_platform.authoring.domain.model.commands;
+
+public record DeleteAuthorCommand(Long authorId) {
+}
---
+++ src/main/java/pe/edu/upc/center/autores_platform/authoring/domain/model/commands/UpdateAuthorCommand.java
@@ -0,0 +1,4 @@
+package pe.edu.upc.center.autores_platform.authoring.domain.model.commands;
+
+public record UpdateAuthorCommand(Long authorId, String name, String nationality, String biography) {
+}
